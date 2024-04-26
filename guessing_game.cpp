393f0cb9d9a6edefd1035284dba2d05ebd8c677d
thales <thalesquadros2@gmail.com>
2024-04-26 09:12:33 -0300

    adding new learns in c++

---
+++ guessing_game.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "********************************\n" << std::endl;
+    std::cout << "  Welcome to the guessing game  \n" << std::endl;
+    std::cout << "********************************\n" << std::endl;
+    
+}
